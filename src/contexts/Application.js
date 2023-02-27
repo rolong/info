6604bcee3fda0ee8615161742058db91c93c9fcf
Rolong <rolong@vip.qq.com>
2023-02-27 15:04:25 +0800

    Fix for health check

--- src/contexts/Application.js
+++ src/contexts/Application.js
@@ -3,8 +3,8 @@ import { timeframeOptions, SUPPORTED_LIST_URLS__NO_ENS } from '../constants'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import getTokenList from '../utils/tokenLists'
-import { healthClient } from '../apollo/client'
-import { SUBGRAPH_HEALTH } from '../apollo/queries'
+// import { healthClient } from '../apollo/client'
+// import { SUBGRAPH_HEALTH } from '../apollo/queries'
 dayjs.extend(utc)
 
 const UPDATE = 'UPDATE'
@@ -172,28 +172,28 @@ export function useLatestBlocks() {
   const latestBlock = state?.[LATEST_BLOCK]
   const headBlock = state?.[HEAD_BLOCK]
 
-  useEffect(() => {
-    async function fetch() {
-      healthClient
-        .query({
-          query: SUBGRAPH_HEALTH,
-        })
-        .then((res) => {
-          const syncedBlock = res.data.indexingStatusForCurrentVersion.chains[0].latestBlock.number
-          const headBlock = res.data.indexingStatusForCurrentVersion.chains[0].chainHeadBlock.number
-          if (syncedBlock && headBlock) {
-            updateLatestBlock(syncedBlock)
-            updateHeadBlock(headBlock)
-          }
-        })
-        .catch((e) => {
-          console.log(e)
-        })
-    }
-    if (!latestBlock) {
-      fetch()
-    }
-  }, [latestBlock, updateHeadBlock, updateLatestBlock])
+  // useEffect(() => {
+  //   async function fetch() {
+  //     healthClient
+  //       .query({
+  //         query: SUBGRAPH_HEALTH,
+  //       })
+  //       .then((res) => {
+  //         const syncedBlock = res.data.indexingStatusForCurrentVersion.chains[0].latestBlock.number
+  //         const headBlock = res.data.indexingStatusForCurrentVersion.chains[0].chainHeadBlock.number
+  //         if (syncedBlock && headBlock) {
+  //           updateLatestBlock(syncedBlock)
+  //           updateHeadBlock(headBlock)
+  //         }
+  //       })
+  //       .catch((e) => {
+  //         console.log(e)
+  //       })
+  //   }
+  //   if (!latestBlock) {
+  //     fetch()
+  //   }
+  // }, [latestBlock, updateHeadBlock, updateLatestBlock])
 
   return [latestBlock, headBlock]
 }
